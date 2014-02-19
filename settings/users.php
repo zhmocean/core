71ab6e4fc11e144409cbcd700eb28966ea081f52
raghunayyar <me@iraghu.com>
2014-02-19 21:23:29 +0530

    resolving conflict when picking 8b81a0b

--- settings/users.php
+++ settings/users.php
@@ -116,7 +116,7 @@ if(!empty($adminGroup)) {
 	array_multisort($sortAdminGroupsKeys, SORT_DESC, $adminGroup);
 }
 
-$tmpl = new OC_Template( "settings", "users", "user" );
+$tmpl = new OC_Template( "settings", "users/main", "user" );
 $tmpl->assign( 'users', $users );
 $tmpl->assign( 'groups', $groups );
 $tmpl->assign( 'adminGroup', $adminGroup );
