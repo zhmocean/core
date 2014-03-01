302e21e9912b67bc97fe9746b12d78141c6ba87c
kondou <kondou@ts.unde.re>
2014-03-01 01:36:04 +0100

    Make scrutinizer happy

--- core/js/maintenance-check.js
+++ core/js/maintenance-check.js
@@ -1,18 +1,20 @@
 // Check every 20 seconds via status.php if maintenance is over
-var timer = window.setInterval(checkStatus, 20000);
+window.setInterval(checkStatus, 20000);
 
 function checkStatus() {
 	var request = new XMLHttpRequest();
-	ocroot = location.pathname.substr(0, location.pathname.indexOf('index.php'));
+	var ocroot = location.pathname.substr(
+		0, location.pathname.indexOf('index.php')
+	);
 	request.open("GET", ocroot+'status.php', true);
 	request.send();
 	request.onreadystatechange = function() {
 		if (request.readyState === 4) {
 			var response = request.responseText;
-			responseobj = JSON.parse(response);
+			var responseobj = JSON.parse(response);
 			if (responseobj.maintenance === 'false') {
 				window.location.reload();
 			}
 		}
-	}
-};
+	};
+}
