d2e3647fb6fc6018d861a7035ae5c977f472155e
Thomas Müller <thomas.mueller@tmit.eu>
2014-05-05 20:15:11 +0200

    fix unit test

--- tests/lib/db/mdb2schemamanager.php
+++ tests/lib/db/mdb2schemamanager.php
@@ -16,14 +16,14 @@ class MDB2SchemaManager extends \PHPUnit_Framework_TestCase {
 		$connection = \OC_DB::getConnection();
 		$manager = new \OC\DB\MDB2SchemaManager($connection);
 
-		$manager->removeDBStructure(__DIR__ . '/ts-autoincrement-before.xml');
-
 		$manager->createDbFromStructure(__DIR__ . '/ts-autoincrement-before.xml');
 		$connection->executeUpdate('insert into `*PREFIX*table` values (?)', array('abc'));
 		$connection->executeUpdate('insert into `*PREFIX*table` values (?)', array('abc'));
 		$connection->executeUpdate('insert into `*PREFIX*table` values (?)', array('123'));
 		$connection->executeUpdate('insert into `*PREFIX*table` values (?)', array('123'));
 		$manager->updateDbFromStructure(__DIR__ . '/ts-autoincrement-after.xml');
+
+		$this->assertTrue(true);
 	}
 
 }
